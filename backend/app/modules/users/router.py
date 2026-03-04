2e0ea9aeb85c4f202dcba45c877b306d200299b9
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-04 15:45:21 +0100

    [DEL] spaces

--- backend/app/modules/users/router.py
+++ backend/app/modules/users/router.py
@@ -31,8 +31,6 @@ async def login(login: LoginRequest, session: Session = Depends(get_session)) ->
         token_type="bearer"
     )
 
-
-
 @router.get("/me", response_model=UserView, tags=["User"])
 async def read_me(current_user: User = Depends(get_current_user)):
     return UserView(
@@ -44,11 +42,9 @@ async def read_me(current_user: User = Depends(get_current_user)):
         created_at=current_user.created_at
     )
 
-
 @router.patch("/me", response_model=UserView, tags=["User"])
 async def patch_me(user_update: UserUpdate, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
     return update_user(session=session, user=current_user, user_update=user_update)
-
 
 @router.delete("/me", tags=["User"])
 async def delete_user(current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
