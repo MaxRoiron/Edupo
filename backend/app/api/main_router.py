68f505082211608cc1f8706052845033f1df5b56
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-04 15:42:34 +0100

    [UPDT] route's tags

--- backend/app/api/main_router.py
+++ backend/app/api/main_router.py
@@ -4,6 +4,6 @@ from fastapi import APIRouter
 
 api_router = APIRouter()
 
-api_router.include_router(users_router, tags=["user"])
+api_router.include_router(users_router)
 api_router.include_router(oauth_router, prefix="/auth", tags=["OAuth"])
-api_router.include_router(user_data_router, tags=["user"])
+api_router.include_router(user_data_router)
