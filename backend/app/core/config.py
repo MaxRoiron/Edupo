7a5bf5cafbabdbc8de7ffc448859d3892315a854
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-05 16:18:35 +0100

    [DEL] useless code

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,12 +3,6 @@ from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=os.getenv("ENV_FILE", os.path.join(os.path.dirname(__file__), "../../.env")),
-        env_file_encoding="utf-8",
-        extra="ignore"
-    )
-
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_SERVER: str
