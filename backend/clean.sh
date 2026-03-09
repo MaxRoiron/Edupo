6d34b41c4c2ef8885eb9557000d95ff4b0fd550a
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-09 17:29:02 +0100

    [ADD] remove coverage files

--- backend/clean.sh
+++ backend/clean.sh
@@ -3,4 +3,6 @@ echo "Nettoyage des fichiers temporaires Python..."
 
 sudo find . -type d -name "__pycache__" -exec rm -rf {} +
 
+rm -f .coverage
+
 echo "Terminé : Tous les dossiers __pycache__ ont été supprimés."
