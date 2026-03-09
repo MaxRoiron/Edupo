46f195d8b265ece9232b3c5530e6956066e0baf9
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-09 14:22:31 +0100

    [UPDT] update router according to tests results

--- backend/app/modules/user_data/router.py
+++ backend/app/modules/user_data/router.py
@@ -15,10 +15,10 @@ async def post_user_data(user_data: UserDataCreate, user: User = Depends(get_cur
     db_user_data = user.user_data
     if db_user_data is not None:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User's data already created")
-    return add_user_data(session=session, user_data=user_data)
+    return add_user_data(session=session, user_data=user_data, user_id=user.id)
 
 @router.get("/me/data", response_model=UserDataView, tags=["User"])
-async def get_user_data(user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+async def get_user_data(user: User = Depends(get_current_user)):
     user_data = user.user_data
     if user_data is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User's data can't be found")
@@ -39,11 +39,11 @@ async def patch_user_data(data: UserDataUpdate, user: User = Depends(get_current
 
 @router.put("/me/data/reset", response_model=UserDataView, tags=["User"])
 async def reset_user_datas(current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
-    return reset_user_data(email=current_user.email, session=session, user=current_user)
+    return reset_user_data(session=session, user_data=current_user.user_data)
 
 @router.put("/admin/{user_email}/reset", response_model=UserDataView, tags=["Admin"])
 async def reset_user_datas(user_email: str, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
-    if current_user.role != "admin":
+    if current_user.role == "user":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You don't have the required authorization")
     user_to_reset = get_user_by_email(session=session, email=user_email)
     return reset_user_data(session=session, user_data=user_to_reset.user_data)
