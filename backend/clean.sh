0e2d138683d7ad0c7399abb2edac94d7c32ef508
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-09 12:27:32 +0100

    [UPDT] script clean backend

--- backend/clean.sh
+++ backend/clean.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Script pour supprimer récursivement tous les dossiers __pycache__
 echo "Nettoyage des fichiers temporaires Python..."
 
-find . -type d -name "__pycache__" -exec rm -rf {} +
+sudo find . -type d -name "__pycache__" -exec rm -rf {} +
 
 echo "Terminé : Tous les dossiers __pycache__ ont été supprimés."
