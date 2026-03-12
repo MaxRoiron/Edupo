0424bab2c891a72c6a38e8ba8a445ff8aca1f4b1
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-12 12:56:20 +0100

    [ADD] all tables includes

--- backend/app/modules/__init__.py
+++ backend/app/modules/__init__.py
@@ -3,6 +3,8 @@ from .external_oauth import oauth_router
 from .institution import Institution, InstitutionType, Power, institution_router
 from .law import Law, Vote, VoteResult
 from .institution import Institution, InstitutionType, Power, institution_router
+from .external_oauth import oauth_router
+from .law import Law, Vote, VoteResult
 from .political_figure import PoliticalFigure, PoliticalRole
 from .political_party import PoliticalParty, Domain, PoliticalProgram, PoliticalTopic, ElectionType, PartyVote
 from .user_data import UserData, ProfessionalStatus, SocialStatus, GenderIdentities, user_data_router
