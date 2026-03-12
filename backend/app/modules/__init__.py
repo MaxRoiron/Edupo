cd308f46ea12a5fd9fb6be9e7a5295e1ba0afdc7
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-12 13:11:54 +0100

    [ADD] partyvote include

--- backend/app/modules/__init__.py
+++ backend/app/modules/__init__.py
@@ -3,7 +3,7 @@ from .external_oauth import oauth_router
 from .institution import Institution, InstitutionType, Power
 from .law import Law, Vote, VoteResult
 from .political_figure import PoliticalFigure, PoliticalRole
-from .political_party import PoliticalParty, Domain, PoliticalProgram, PoliticalTopic, ElectionType
+from .political_party import PoliticalParty, Domain, PoliticalProgram, PoliticalTopic, ElectionType, PartyVote
 from .user_data import UserData, ProfessionalStatus, SocialStatus, GenderIdentities, user_data_router
 from .user_vote import UserVote
 from .users import User, users_router
