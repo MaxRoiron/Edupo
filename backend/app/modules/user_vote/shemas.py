f35b77f41e29842b2cade84ef9720c2951c511b8
T4NkYF3R <nicolas.negron@epitech.eu>
2026-03-12 13:22:10 +0100

    [UPDT] shema name

--- backend/app/modules/user_vote/shemas.py
+++ backend/app/modules/user_vote/shemas.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel
 
-class UserVote(BaseModel):
+class UserVoteCreate(BaseModel):
     user_id: int
     law_id: int
     position_id: int
 
-class UserVote(BaseModel):
+class UserVoteUpdate(BaseModel):
     user_id: int | None = None
     law_id: int | None = None
     position_id: int | None = None
 
-class UserVote(BaseModel):
+class UserVoteView(BaseModel):
     id: int
     user_id: int
     law_id: int
